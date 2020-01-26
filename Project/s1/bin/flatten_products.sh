dcf64041741d8bbcbecb18893e1292474de1b95d
Lokesh0721 <60285768+Lokesh0721@users.noreply.github.com>
2020-01-26 13:07:14 +0530

    Committing..

--- Project/s1/bin/flatten_products.sh
+++ Project/s1/bin/flatten_products.sh
@@ -1,25 +1,15 @@
 export runtime=`date -u +"%Y%m%d%H%M%S"`
-export log_file=/home/cloudera/Batch-2/project/s1/logs/products'_'$runtime.log
+
+export log_file=/home/cloudera/project/s1/logs/products'_'$runtime.log
 
 exec &> ${log_file}
 
 echo "execution started..."
 
-pig -x local -f /home/cloudera/Batch-2/project/s1/pig/flattern_products.pig
-
-if [ $? -eq 0 ]
-
-then
-	echo "Pig Command Executed Successfully!"
-else 
-	echo "Pig Command Execution Failed, Please check !!"
-fi
+pig -x local -f /home/cloudera/project/s1/pig/flatten_products.pig
 
 echo "process completed!"
 
-mv /home/cloudera/Batch-2/project/s1/srcfiles/lz/prod*  /home/cloudera/Batch-2/project/s1/srcfiles/archive
+mv /home/cloudera/project/s1/srcfiles/lz/prod*  /home/cloudera/project/s1/srcfiles/archive
 
 echo "file moved to  archive!"
-
-
-
--- Project/s1/bin/flatten_transactions.sh
+++ Project/s1/bin/flatten_transactions.sh
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-//Test
-
 export runtime=`date -u +"%Y%m%d%H%M%S"`
 
 export log_file=/home/cloudera/project/s1/logs/transactions'_'$runtime.log
@@ -16,28 +13,4 @@ echo "process completed!"
 mv /home/cloudera/project/s1/srcfiles/lz/tran*  /home/cloudera/project/s1/srcfiles/archive
 
 echo "file moved to  archive!"
-=======
-export runtime=`date -u +"%Y%m%d%H%M%S"`
-export logfile=/home/cloudera/Batch-2/project/s1/logs/transactions'_'$runtime.log
-exec &> ${logfile}
-
-echo "Script Started "
-
-pig -x local -f /home/cloudera/Batch-2/project/s1/pig/flattern_transactions.pig
-
-if [$? -eq 0 ]
-
-then 
-	echo "Pig Command Executed Successfully !"
-	
-	mv /home/cloudera/Batch-2/project/s1/srcfiles/lz/trans* /home/cloudera/Batch-2/project/s1/srcfiles/archive
-
-else
-	echo "Pig Command Failed, Please check !"
-
-fi
-
-echo "Script Completed !"
-
->>>>>>> origin/master
 
